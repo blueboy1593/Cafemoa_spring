b93d0c24fb0c29a9add0b116b539ebbf6115edae
Dolphago <37108728+adamdoha@users.noreply.github.com>
2020-02-10 20:16:46 +0900

    아오..

--- Back/Latte/src/main/java/com/latte/admin/WebConfig.java
+++ Back/Latte/src/main/java/com/latte/admin/WebConfig.java
@@ -4,10 +4,26 @@ import com.latte.admin.service.jwt.JwtInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/WEB-INF/resources/");
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
+
+
+
     private static final String[] EXCLUDE_PATHS = {
 //            "swagger-ui.html#",
 //            "/swagger-resources",
